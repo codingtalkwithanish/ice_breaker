9042e906b75c2674acab59c0a504e6f0ae148431
codingtalkwithanish <fusebulbs.com@gmail.com>
2024-08-01 18:02:35 +0530

    commit

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,20 +4,15 @@ from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
 from langchain_core.output_parsers.string import StrOutputParser
 import os
+from third_parties.linkedin import scrap_linkedin_profile
 
-information = """Narendra Damodardas Modi  born 17 September 1950)[a] is an Indian politician serving as
- the current Prime Minister of India since 26 May 2014. Modi was the chief minister of 
- Gujarat from 2001 to 2014 and is the Member of Parliament (MP) for Varanasi. He is a member 
- of the Bharatiya Janata Party (BJP) and of the Rashtriya Swayamsevak Sangh (RSS), a
-   right wing Hindu nationalist paramilitary volunteer organisation. He is the longest-serving
-     prime minister outside the Indian National Congress.   
-Modi was born and raised in Vadnagar in northeastern Gujarat, where he completed his secondary education. He was introduced to the RSS at the age of eight. At the age of 18, he was married to Jashodaben Modi, whom he abandoned soon after, only publicly acknowledging her four decades later when legally required to do so. Modi became a full-time worker for the RSS in Gujarat in 1971. The RSS assigned him to the BJP in 1985 and he rose through the party hierarchy, becoming general secretary in 1998.[b] In 2001, Modi was appointed Chief Minister of Gujarat and elected to the legislative assembly soon after. His administration is considered complicit in the 2002 Gujarat riots,[c] and has been criticised for its management of the crisis. According to official records, a little over 1,000 people were killed, three-quarters of whom were Muslim; independent sources estimated 2,000 deaths, mostly Muslim.[12] A Special Investigation Team appointed by the Supreme Court of India in 2012 found no evidence to initiate prosecution proceedings against him.[d] While his policies as chief minister were credited for encouraging economic growth, his administration was criticised for failing to significantly improve health, poverty and education indices in the state.[e]"""
+
 if __name__ == "__main__":
     load_dotenv()
     print("Hello LangChain")
     print(os.environ["OPENAI_API_KEY"])
 
-    summary_template = """Given the information {information} about a person from i want you to create:
+    summary_template = """Given the linked information {information} about a person from i want you to create:
                         1.a short summary
                         2.two interesting facts about them
                         """
@@ -28,5 +23,6 @@ if __name__ == "__main__":
     llm=ChatOllama(model="llama3")
 
     chain = summary_prompt_template | llm | StrOutputParser()
+    information = scrap_linkedin_profile(linkedin_profile_url='https://www.linkedin.com/in/anish-singh-32586540/')
     res = chain.invoke(input={"information": information})
     print(res)
--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -7,3 +7,40 @@ load_dotenv()
 def scrap_linkedin_profile(linkedin_profile_url:str,mock:bool=False):
     """scrape information from the linkedin profiles,
     Manually scrape the information from the linkedin profile"""
+
+    if mock:
+        linkedin_profile_url="https://gist.githubusercontent.com/codingtalkwithanish/03c4efe297448b9f87c86c31de1879ea/raw/600fe99def27dee654d1e100242dbf66745d34f0/anish-singh.json"
+        response=requests.get(linkedin_profile_url,timeout=10)
+
+    else:
+        api_key = os.environ.get("api_key")
+        headers = {'Authorization': 'Bearer ' + api_key}
+        api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
+        params = {
+        #'linkedin_profile_url': 'https://www.linkedin.com/in/anish-singh-32586540/',
+        'url':linkedin_profile_url,
+        'github_profile_id': 'include',
+        'personal_email': 'include',
+        'inferred_salary': 'include',
+        'skills': 'include',
+        'use_cache': 'if-present',
+        'fallback_to_cache': 'on-error',
+        }
+        response = requests.get(api_endpoint,
+        params=params,
+        headers=headers,timeout=10)
+    
+    data=response.json()
+    
+    
+    data={k:v for k,v in data.items() if v not in ([],"",None) and k not in ["people_also_viewed"] }
+
+    if data.get("groups"):      
+         for group_dict in data.get("groups"):
+              group_dict.pop("profile_pic_url")
+    return data
+
+if __name__=="__main__":
+    print(
+        scrap_linkedin_profile(linkedin_profile_url="https://www.linkedin.com/in/anish-singh-32586540/",mock=True)
+          )
